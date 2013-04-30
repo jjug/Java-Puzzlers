e58ce34425188eb526f17c5ca64e7866a96e5dd8
Takayuki Okazaki <takayuki.okazaki@gree.net>
2013-04-30 16:55:16 +0900

    fix number

--- Population/Population.java
+++ Population/Population.java
@@ -7,7 +7,7 @@ public class Population {
         Map<String, Integer> population2012 = new HashMap<>();
 
         population2011.put("世田谷区", 835_819); population2012.put("世田谷区", 831_654);
-        population2011.put("練馬区", 693_368); population2012.put("練馬区", 693_368);
+        population2011.put("練馬区", 693_368); population2012.put("練馬区", 692_450);
         population2011.put("大田区", 674_920); population2012.put("大田区", 674_527);
         population2011.put("江戸川区", 654_537); population2012.put("江戸川区", 651_884);
         population2011.put("足立区", 644_448); population2012.put("足立区", 641_888);
@@ -17,8 +17,7 @@ public class Population {
         population2011.put("葛飾区", 435_253); population2012.put("葛󠄁飾区", 431_796);
         population2011.put("品川区", 351_350); population2012.put("品川区", 348_590);
 
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE, max = 0;
         for (String ku : population2011.keySet()) {
             int change = Math.abs(population2012.get(ku) - population2011.get(ku));
             if (change < min) min = change;
