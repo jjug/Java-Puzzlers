ef51896ac1eecab0d40ab052fd9a8896713408cb
Takayuki Okazaki <takayuki.okazaki@gree.net>
2013-04-30 11:28:17 +0900

    HelloWorld2

---
+++ HelloWorld/HelloWorld2.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class HelloWorld2 {
+    public static void main(String... args) {
+        try (OutputStreamWriter sw = new OutputStreamWriter(System.out)) {
+            sw.write("Hello");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("World");
+    }
+}
+
+/*
+ * 実行するとどうなるでしょうか?
+ * (1) Hello
+ * (2) World
+ * (3) HelloWorld
+ * (4) java.io.IOExceptionが発生する
+ */
